ac9c2ba3a4d9f4f4e7a8c637efddb27dc26247d3
Rievaldhi <77229263+Rievaldhi@users.noreply.github.com>
2021-08-25 20:38:39 +0700

    test number 2
    create 5 testcase in 1 file js

---
+++ test2.js
@@ -0,0 +1,40 @@
+describe('Test ke - 2', function () {
+    it('tc-1', function(){
+        cy.visit('https://ultimateqa.com/simple-html-elements-for-automation/')
+        cy.get('#et_pb_contact_name_0').type('Rievaldhi')
+        cy.get('#et_pb_contact_email_0').type('rievaldhi@gmail.com')
+        cy.get('.et_pb_contact_submit').click()
+        cy.get('#simpleElementsLink').click()
+        cy.get('.entry-title').contains('Link success')
+    }) ,
+    it(' tc-2 ' , function () {
+
+        cy.visit('https://ultimateqa.com/simple-html-elements-for-automation/')
+        cy.get('[value="male"]').click()
+                
+    }) , 
+    it(' tc-3 ' , function () {
+
+        cy.visit('https://ultimateqa.com/simple-html-elements-for-automation/')
+        cy.get('[value="Bike"]').click()
+        cy.get('[value="Car"]').click()
+                
+    }) , 
+    it(' tc-4 ' , function () {
+
+        cy.visit('https://ultimateqa.com/simple-html-elements-for-automation/')
+        cy.get('select').select('Audi')       
+                
+    }) , 
+    it(' tc-5 ' , function () {
+
+        cy.visit('https://ultimateqa.com/simple-html-elements-for-automation/')
+        cy.get('.et_pb_cta_0 > .et_pb_button_wrapper > .et_pb_button').click()
+        cy.get('.entry-title').contains('Button success')
+                
+    })
+
+    Cypress.on('uncaught:exception', (err, runnable)=>{
+        return false
+    })
+})
